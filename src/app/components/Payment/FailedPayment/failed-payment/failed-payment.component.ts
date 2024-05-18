ae32a506c1d0765d2e9d97260719f61a0b6ab9cd
PoulaMaher <poula.maher199@gmail.com>
2024-05-18 18:10:15 +0300

    init payment Commponent

---
+++ src/app/components/Payment/FailedPayment/failed-payment/failed-payment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-failed-payment',
+  standalone: true,
+  imports: [],
+  templateUrl: './failed-payment.component.html',
+  styleUrl: './failed-payment.component.css'
+})
+export class FailedPaymentComponent {
+
+}
